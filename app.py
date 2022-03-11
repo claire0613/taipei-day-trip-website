f8b929569dc80941040afa1e8ce6ce051a8dc2e1
claire Liang <claire0711@gmail.com>
2022-03-11 17:34:45 +0800

    upload-guide2

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def thankyou():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=3000,debug=True)
+    app.run(host='0.0.0.0',port=3000)
--- database.py
+++ database.py
@@ -5,8 +5,7 @@ import json
 import os
 from dotenv import load_dotenv
 load_dotenv()
-try:
-    dbconfig = {
+dbconfig = {
         'host': os.getenv("SERVER_HOST"),
         'user': os.getenv("SERVER_USER"),
         'password': os.getenv("SERVER_PASSWORD"),
@@ -15,26 +14,28 @@ try:
         
     }
 
-    connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(
         pool_name="pool",
         pool_size=5,
         pool_reset_session=True,
         **dbconfig
     )
-    connection = connection_pool.get_connection()
-    mycursor = connection.cursor()
-
-
-except Error as e:
-    print("Error while connecting to MySQL using Connection pool ", e)
-
-finally:
-    if connection.in_transaction:
-        connection.rollback()
-
-
+def connection_db(sql,value):
+    try:
+        connection = connection_pool.get_connection()
+        mycursor = connection.cursor()
+        mycursor.execute(sql, value)
         
+    except Error as e:
+        print("Error while connecting to MySQL using Connection pool ", e)
 
+    finally:
+        result=mycursor.fetchall()
+        if connection.in_transaction:
+            connection.rollback()
+        mycursor.close()
+        connection.close()
+        return result
 
 
 # for api/attractions
@@ -43,15 +44,14 @@ def attraction_count(**data):
     if data['keyword']:
         sql="SELECT count(name) FROM attractions WHERE name LIKE %s "
         value=('%'+data['keyword']+'%',)
-        mycursor.execute(sql,value)
     
     else:
         sql="SELECT count(name) FROM attractions"
-        mycursor.execute(sql)
-        
-    allcount=mycursor.fetchall()
+        value=()
+    allcount=connection_db(sql,value)
   
     if  allcount:
+        
         return  int(allcount[0][0])
 
 
@@ -63,33 +63,45 @@ def search_attracion(**data):
     else:
         sql = "SELECT * FROM attractions LIMIT %s , %s" 
         value=(page_12,12)
-    mycursor.execute(sql,value)
-    result = mycursor.fetchall()
+    result = connection_db(sql,value)
     if result:
         lst = []
         for i in result:
-            item = dict(zip(mycursor.column_names, i))
-            item ["images"] = json.loads(item["images"])
+            item=resultformat(i)
             lst.append(item)
+        print(lst[0]["images"])
         return lst
     else:
         return None
 
+def resultformat(result):
+    result = {
+        "id":result[0],
+        "name":result[1],
+        "category":result[2],
+        "descripition":result[3],
+        "address":result[4],
+        "transport":result[5],
+        "mrt":result[6],
+        "latitude":result[7],
+        "longitude":result[8],
+        "images":json.loads(result[9])
+    }
+    return result
 
+search_attracion(page=0,keyword=None)
 
 
 def search_attractionid(data):
   
     sql="SELECT * FROM attractions WHERE id = %s"
     value=(data,)
-    mycursor.execute(sql,value)
-    result = mycursor.fetchall()
-    for i in result:
-        item = dict(zip(mycursor.column_names, i))
-        item ["images"] = json.loads(item["images"])
-
+    result =  connection_db(sql,value)
+    if result:
+        for i in result:
+            item=resultformat(i)
         return item
     else:
         return None
     
-    
+
